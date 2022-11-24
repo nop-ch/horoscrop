7096f9f3d157c4916f7c3378b14e918d43b9e9f5
nop-ch <noppon.ji@chococrm.com>
2022-11-24 22:25:50 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<script setup>
+  import { ref } from 'vue'
+  import  CardHoroScrope  from './cards/CardHoroScrope.vue'
+  defineProps({
+    msg: String
+  })
+  const message = ref()
+
+  console.log(message.value);
+  function onInput(event){
+    console.log(event.target.tagName)
+  }
+</script>
+
+<template>
+  <section>
+    <div class="showcase" >
+      <div class="absolute" style="z-index: 1;">
+        <CardHoroScrope />
+      </div>
+        <video  autoplay muted loop id="myVideo">
+          <source src="../assets/videos/nightsky.mp4" type="video/mp4">
+          <source src="movie.ogg" type="video/ogg">
+          Your browser does not support the video tag.
+        </video>
+    </div>
+
+  </section>
+</template>
+
+<style scoped>
+.video-container{
+  position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+
+}
+#myVideo {
+  min-width: 100%;
+	min-height: 100%;
+
+  width: 100vw;
+  position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	object-fit: cover;
+}
+
+.showcase {
+	height: 100vh;
+}
+</style>
---
+++ src/components/cards/CardHoroScrope.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+   <div class="h-full w-full d-flex justify-content-center align-items-center text-center text-light">
+         <div style="display:flex ">
+            <div class="col ">
+               <div class="card" >
+                  <img src="../../assets/images/card-1.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">1111111111111111111111111111111111111111111111111111111111Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+               </div>
+            </div>
+            <div class="col">
+               <div class="card" >
+                  <img src="../../assets/images/card-2.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     <a href="#" class="btn btn-primary  mt-auto">Go somewhere</a>
+                  </div>
+               </div>
+            </div>
+            <div class="col">
+               <div class="card" >
+                  <img src="../../assets/images/card-3.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     <a href="#" class="btn btn-primary  mt-auto">Go somewhere</a>
+                  </div>
+               </div>
+            </div>
+
+         </div>
+     
+   </div>
+</div>
+</template>
+
+<script>
+export default {
+   getImgUrl(image) {
+    return `../../assets/images/${image}.png`
+   }   
+}
+</script>
+
+<style scoped>
+   .card{
+      width: 320px;    
+      height: 100%;
+      margin: 0 auto;
+      /* background-color: rgba(132, 101, 255, 0.428); */
+      background-color: rgba(132, 101, 255, 0.7);
+   }
+   .btn-purple{
+      background-color: rgb(175, 21, 230);
+   }
+   .card-body{
+      display: flex;
+      flex-direction: column;
+   }
+
+</style>
